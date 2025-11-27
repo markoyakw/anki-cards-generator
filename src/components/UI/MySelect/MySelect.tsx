701f4623e7d7b9012561a8ed6f2aaa860ecd6b3b
Marko <yakovenkomarko@gmail.com>
2025-11-27 16:22:35 +0100

    Fix select native compatibility issues (black rectangle when there are no option html tags as children)

--- src/components/UI/MySelect/MySelect.tsx
+++ src/components/UI/MySelect/MySelect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type FC, type OptionHTMLAttributes, type ReactElement, type SelectHTMLAttributes } from "react"
+import { useEffect, useRef, useState, type FC, type OptionHTMLAttributes, type ReactElement, type SelectHTMLAttributes } from "react"
 import classes from "./MySelect.module.css"
 
 type TSelectChild = ReactElement<OptionHTMLAttributes<HTMLOptionElement> & { value: string }>
@@ -11,8 +11,10 @@ const MySelect: FC<TSelectProps> = ({ children, ...props }) => {
 
     const [isSelectOpen, setIsSelectOpen] = useState(false)
     const [value, setValue] = useState(children[0].props.value)
+    const selectRef = useRef(null)
 
-    const onSelectBlur = () => {
+    const onSelectBlur = (e: MouseEvent) => {
+        if (e.target === selectRef.current) return
         setIsSelectOpen(false)
     }
     const onSelectClick = () => {
@@ -22,22 +24,27 @@ const MySelect: FC<TSelectProps> = ({ children, ...props }) => {
         setValue(newValue)
     }
 
+    useEffect(() => {
+        window.addEventListener("click", onSelectBlur)
+    }, [])
+
     const selectWrapperOpenClassName = isSelectOpen ? classes["select__wrapper--open"] : ""
     const selectMenuDropdownClassName = isSelectOpen ? "" : classes["select__menu-dropdown--closed"]
 
     return (
         <div className={`${classes["select__wrapper"]} ${selectWrapperOpenClassName}`}>
-            <select {...props} className={classes["select"]}
+            {/* readable select with options for screen readers */}
+            <select {...props} className={classes["select"]} ref={selectRef}
                 onClick={() => onSelectClick()}
-                onBlur={() => onSelectBlur()}
+                onMouseDown={e => e.preventDefault()}
+                // prevent opening a native select menu
                 value={value}
-                // onMouseDown={e => e.preventDefault()}
             >
                 {children}
             </select>
-            <ul onMouseDown={e => e.preventDefault()}
-                className={`${classes["select__menu-dropdown"]} ${selectMenuDropdownClassName}`}
-            >
+
+            {/* styled select drop menu as a workaround for css limitations*/}
+            <ul className={`${classes["select__menu-dropdown"]} ${selectMenuDropdownClassName}`}>
                 {children.map((option, optionId) =>
                     <li key={optionId} className={classes["select__option"]} onClick={() => onOptionChoose(option.props.value)}>
                         {option}
